186d137396193f994d8f18367dcc4183b0ef39aa
Genesio Lini <genesio.lini@pragma-servizi.it>
2019-01-21 22:02:47 +0100

    main functions implemented

--- client/client.go
+++ client/client.go
@@ -1,5 +1,14 @@
 package client
 
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"encoding/xml"
+	"errors"
+	"io"
+	"strings"
+)
+
 var countryURLs = map[string]string{
 	`IT`: `https://eidas.agid.gov.it/TL/TSL-IT.xml`,
 }
@@ -27,10 +36,93 @@ var serviceExtensions = map[string]string{
 
 type client struct {
 	country string
+	certs   TrustServiceStatusList
 }
 
 func NewClient(country string) *client {
 	return &client{
 		country: country,
+		certs:   TrustServiceStatusList{},
 	}
+}
+
+func (c client) URL() string {
+	return countryURLs[c.country]
+}
+
+func (c *client) Decode(reader io.Reader) error {
+	dec := xml.NewDecoder(reader)
+	return dec.Decode(&c.certs)
+}
+
+func (c client) ExtractCertificates(x509Certs chan<- *x509.Certificate) error {
+	defer close(x509Certs)
+
+	for _, provider := range c.certs.TrustServiceProviderList.TrustServiceProvider {
+		for _, srv := range provider.TSPServices.TSPService {
+			xmlSvcInfo := srv.ServiceInformation
+
+			if xmlSvcInfo.ServiceTypeIdentifier != serviceTypes[`CA/QC`] {
+				continue
+			}
+
+			if xmlSvcInfo.ServiceStatus != serviceStatuses[`granted`] {
+				continue
+			}
+
+			extensionFound := false
+			for _, ext := range xmlSvcInfo.ServiceInformationExtensions.Extension {
+				if ext.AdditionalServiceInformation.URI.Text == serviceExtensions[`eSignatures`] {
+					extensionFound = true
+					break
+				}
+			}
+
+			if !extensionFound {
+				continue
+			}
+
+			for _, digitalId := range xmlSvcInfo.ServiceDigitalIdentity.DigitalId {
+
+				if digitalId.X509Certificate == `` {
+					continue
+				}
+
+				pemText := "-----BEGIN CERTIFICATE-----\n"
+				pemText += strings.Join(chunkString(digitalId.X509Certificate, 64), "\n")
+				pemText += "\n-----END CERTIFICATE-----\n"
+
+				block, rest := pem.Decode([]byte(pemText))
+				if block == nil || len(rest) > 0 {
+					return errors.New("Certificate decoding error")
+				}
+				cert, err := x509.ParseCertificate(block.Bytes)
+				if err != nil {
+					return err
+				}
+
+				x509Certs <- cert
+			}
+		}
+	}
+
+	return nil
+}
+
+func chunkString(s string, chunkSize int) []string {
+	var chunks []string
+	runes := []rune(s)
+
+	if len(runes) == 0 {
+		return []string{s}
+	}
+
+	for i := 0; i < len(runes); i += chunkSize {
+		nn := i + chunkSize
+		if nn > len(runes) {
+			nn = len(runes)
+		}
+		chunks = append(chunks, string(runes[i:nn]))
+	}
+	return chunks
 }
--- cmd/agid_ca_updater/main.go
+++ cmd/agid_ca_updater/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"flag"
-	"github.com/systemlogic/agid-ca-updater/client"
+	"fmt"
+	"github.com/SystemLogicIt/agid-ca-updater/client"
 	"log"
+	"net/http"
+	"os"
+)
+
+// Extra ASN1 OIDs that we may need to handle
+var (
+	oidEmailAddress                 = []int{1, 2, 840, 113549, 1, 9, 1}
+	oidExtensionAuthorityInfoAccess = []int{1, 3, 6, 1, 5, 5, 7, 1, 1}
+	oidNSComment                    = []int{2, 16, 840, 1, 113730, 1, 13}
 )
 
 var country = flag.String("country", "IT", "country code to fetch")
@@ -11,8 +24,76 @@ var country = flag.String("country", "IT", "country code to fetch")
 func main() {
 	flag.Parse()
 
-	client := client.NewClient(*country)
+	cli := client.NewClient(*country)
 
-	log.Printf(`[+] Downloading remote file from: %s\n`, client)
+	log.Printf("[+] Downloading remote file from: %s\n", cli.URL())
 
+	certsChan := make(chan *x509.Certificate, 100)
+
+	go func() {
+		// Get the data
+		resp, err := http.Get(cli.URL())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		err = cli.Decode(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = cli.ExtractCertificates(certsChan)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	outputFile, err := os.Create(`ca-bundle-it.crt`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputFile.Close()
+
+	for cert := range certsChan {
+
+		log.Println(printName(cert.Subject.Names))
+
+		block := &pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}
+		err = pem.Encode(outputFile, block)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Printf("[+] done\n")
+}
+
+func printName(names []pkix.AttributeTypeAndValue) []string {
+	values := []string{}
+	for _, name := range names {
+		oid := name.Type
+		if len(oid) == 4 && oid[0] == 2 && oid[1] == 5 && oid[2] == 4 {
+			switch oid[3] {
+			case 3:
+				values = append(values, fmt.Sprintf("CN=%s", name.Value))
+			case 6:
+				values = append(values, fmt.Sprintf("C=%s", name.Value))
+			case 8:
+				values = append(values, fmt.Sprintf("ST=%s", name.Value))
+			case 10:
+				values = append(values, fmt.Sprintf("O=%s", name.Value))
+			case 11:
+				values = append(values, fmt.Sprintf("OU=%s", name.Value))
+			default:
+				values = append(values, fmt.Sprintf("UnknownOID=%s", name.Type.String()))
+			}
+		} else if oid.Equal(oidEmailAddress) {
+			values = append(values, fmt.Sprintf("emailAddress=%s", name.Value))
+		} else {
+			values = append(values, fmt.Sprintf("UnknownOID=%s", name.Type.String()))
+		}
+	}
+
+	return values
 }
